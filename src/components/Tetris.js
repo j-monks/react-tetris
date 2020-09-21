d4c0c3457beb2842d7b56570a2af626f08bfa884
James Monks <J.monks23@gmail.com>
2020-09-21 13:49:42 +0100

    added basic structure of directional movement for the player thats being picked up by the StyledTetrisWrapper

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { createStage } from '../gameHelpers';
+
 //Styled components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
@@ -19,12 +21,51 @@ const Tetris = () => {
     // TELLS US IF THE GAME WHETHER THE GAME IS OVER OR NOT (TRUE/FALSE)
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
     const [stage, setStage] = useStage(player);
 
     console.log('re-render');
+
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y: 0 });
+    }
+
+    const startGame = () => {
+        // RESETS EVERYTHING
+        setStage(createStage());
+        resetPlayer();
+    }
+    
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false })
+
+    }
+    
+    const dropPlayer = () => {
+        drop();
+
+    } 
+
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37){
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            }
+        }
+
+    }
+
+
+
+
+
     return (
-        <StyledTetrisWrapper>
+        // STYLEDTETRISWRAPPER IS RESPONSIBLE FOR THE REGISTERING THE KEYSTROKES OF THE USER
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
             <Stage stage={stage} />
             <aside>
@@ -37,7 +78,7 @@ const Tetris = () => {
                 <Display text="Level"/>
                 </div>
                 )}
-                <StartButton />
+                <StartButton onCick={startGame} />
             </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
