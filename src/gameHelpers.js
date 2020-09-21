0fafaca10799f62480a21ced5440a61749b49b53
James Monks <J.monks23@gmail.com>
2020-09-21 10:12:33 +0100

    rendering cell components in browser from createstage function dimensions

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -6,8 +6,8 @@ export const STAGE_HEIGHT = 20;
 // FOR EACH ROW WE CREATE A NEW ARRAY FROM THE STAGE WIDTH AND 
 // WE FILL IT WITH ANOTHER ARRAY
 export const createStage = () =>
- console.log(Array.from(Array(STAGE_HEIGHT), () =>
+ Array.from(Array(STAGE_HEIGHT), () =>
  new Array(STAGE_WIDTH).fill([0, 'clear'])  
- ));
+ );
 
 
